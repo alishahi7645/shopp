edca72abac9f497087d4bcde5f13ea206e82a0fa
alishahi <shahy8227@gmail.com>
2025-01-19 17:38:08 +0330

    end code header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,39 @@ import { FaShoppingBag } from "react-icons/fa";
 import Menu from './svg/bars-solid.svg';
 import Close from './svg/times-solid.svg';
 function Header() {
+     const [menu , setMenu] = useState(false)
+
+     const toggleMenu = () => {
+          setMenu(!menu)
+     }
+
+     const styleMenu = {
+          right: menu ? 0 : '-100%'
+     }
 
   return (
-     <div>header</div>
+     <header>
+          <div className='cart-icon'>
+               <span>3</span>
+               <Link to='/'><FaShoppingBag/></Link>
+          </div>
+          <ul style={styleMenu}>
+               <li><Link to='/'>خانه</Link></li>
+               <li><Link to='/'>محصولات</Link></li>
+               <li><Link to='/'>درباره ما</Link></li>
+               <li><Link to='/'>تماس باما</Link></li>
+               <li><Link to='/'>ثبت نام و ورود</Link></li>
+               <li>
+                    <img src={Close} width='30' className='menu' onClick={toggleMenu}/>
+               </li>
+          </ul>
+          <div className='logo'>
+               <Link to='/'>alishop</Link>
+          </div>
+          <div className='menu' onClick={toggleMenu}>
+               <img src={Menu} width='30' />
+          </div>
+     </header>
   )
 }
 
